3c43738176f299c956a87e11064d881faed80144
npatne <1.nishadpatne@gmail.com>
2021-06-12 20:52:45 +0530

    basic ui completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaEthereum } from "react-icons/fa";
+import { BsChevronDown, BsPlus, BsDash } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="section">
+      <section className="container">
+        <header>
+          <FaEthereum className="icon active" />
+          <h3>@{"main-net"}</h3>
+          <select name="account" value={"bla"}>
+            <option value="bla">BLA</option>
+            <option value="blah">BLAH</option>
+          </select>
+        </header>
+        <article className="balance-container">
+          <h2>BALANCE</h2>
+          <div className="balance">{"123.443468"}</div>
+        </article>
+        <article className="action-container">
+          <p className="success showi"> 343 wei recieved</p>
+          <p className="warning showi"> 343 wei sent</p>
+          <p className="danger showi">{"error message"}</p>
+          <form
+            className="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="form-control">
+              <label htmlFor="amount">Amount in Wei</label>
+              <input
+                type="text"
+                placeholder="enter amount here"
+                name="amount"
+                id="amount"
+                value={""}
+                onChange={() => {}}
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="to">Send to</label>
+              <input
+                type="text"
+                placeholder="enter address here"
+                name="address"
+                id="to"
+                value={""}
+                onChange={() => {}}
+              />
+            </div>
+            <button type="button" onClick={() => {}}>
+              Send
+            </button>
+            <p
+              className="copy-to-clip"
+              onClick={() => {
+                navigator.clipboard.writeText("account address");
+              }}
+            >
+              click here to copy your account address
+            </p>
+          </form>
+          <div className="transaction-container">
+            <div className="heading">
+              <h2>
+                <strong>Transactions</strong>
+              </h2>
+              <button type="button" className="toggle-btn close">
+                <BsChevronDown />
+              </button>
+            </div>
+            <div className="transactions show-transactions">
+              <div className="transaction">
+                <div className="indicator">
+                  {false ? (
+                    <BsDash className="sent" />
+                  ) : (
+                    <BsPlus className="recieved" />
+                  )}
+                </div>
+                <span>
+                  {" "}
+                  <strong>{"amt"}</strong> wei from <strong>{"xx1"}</strong> to{" "}
+                  <strong>{"you"}</strong>
+                </span>
+              </div>
+
+              <div className="transaction">
+                <div className="indicator">
+                  {true ? (
+                    <BsDash className="sent" />
+                  ) : (
+                    <BsPlus className="recieved" />
+                  )}
+                </div>
+                <span>
+                  {" "}
+                  <strong>{"amt"}</strong> wei from <strong>{"xx1"}</strong> to{" "}
+                  <strong>{"you"}</strong>
+                </span>
+              </div>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
   );
 }
 
